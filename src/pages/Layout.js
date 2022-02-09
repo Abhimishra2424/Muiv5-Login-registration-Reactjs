2b1ff18750bf834f227ccf3eb2eb2937ece3629c
Abhishek <abmishra700@gmail.com>
2022-02-09 15:34:12 +0530

    added layout and routers

---
+++ src/pages/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Home Page</h1>
+    </>
+  )
+}
+
+export default Layout
